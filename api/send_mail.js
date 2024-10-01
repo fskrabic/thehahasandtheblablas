e14041808c48ceb690daa73042c88f60fcce4de6
F <filip.skrabic@protonmail.com>
2024-10-01 09:36:37 +0200

    use env variables

--- api/send_mail.js
+++ api/send_mail.js
@@ -1,37 +1,31 @@
 const nodemailer = require("nodemailer");
 
-// Export an async function that handles the request
 export default async function (req, res) {
   if (req.method === "POST") {
     const { name, title, email, inquiry, message } = req.body;
 
-    // Validate inputs
     if (!name || !title || !inquiry || !message) {
       return res.status(400).json({ error: "Missing required fields" });
     }
 
     try {
-      // Create a transporter using Gmail SMTP
       const transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
-          user: "skrabicf@gmail.com", // Replace with your Gmail email
-          pass: "lhoh jbcm zezf yagg", // Replace with your App Password
+          user: process.env.GMAIL_USER,
+          pass: process.env.GMAIL_APP_PASSWORD,
         },
       });
 
-      // Create the email options
+      const type = inquiry === "booking" ? "[BOOKING REQUEST]" : "[GENERAL]";
       const mailOptions = {
-        from: "skrabicf@gmail.com", // Sender email
-        to: "skrabicf@gmail.com", // Your email (recipient)
-        subject: `${inquiry} - ${title}`, // Email subject
-        text: `Name: ${name}\nEmail: ${email}\nType of Inquiry: ${inquiry}\n\nMessage:\n${message}`,
+        from: process.env.GMAIL_USER,
+        to: process.env.GMAIL_USER,
+        subject: `${type} ${title}`,
+        text: `Sender: ${name}\nEmail: ${email}\n\n\n${message}`,
       };
 
-      // Send the email
       await transporter.sendMail(mailOptions);
-
-      // Send a success response
       res.status(200).json({ message: "Email sent successfully!" });
     } catch (error) {
       console.error("Error sending email:", error);
