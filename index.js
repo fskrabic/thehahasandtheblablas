4101ee7dd73b89072747bde83f10c229473ec4cc
F <filip.skrabic@protonmail.com>
2024-10-09 20:21:03 +0200

    added lyrics, updated styles, new gig

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ document.addEventListener("DOMContentLoaded", () => {});
 const tabs = document.querySelectorAll(".tab");
 const tabContents = document.querySelectorAll(".tab-content");
 
+const hoverContainer = document.querySelector(".hover-container");
+const hoverImage = document.querySelector(".hover-image");
+
+hoverContainer.addEventListener("touchstart", function () {
+  hoverImage.classList.add("active"); // Show image
+});
+
+hoverContainer.addEventListener("touchend", function () {
+  hoverImage.classList.remove("active"); // Hide image
+});
 tabs.forEach((tab) => {
   tab.addEventListener("click", function (e) {
     e.preventDefault();
@@ -30,6 +40,28 @@ document.querySelectorAll("table.interactive").forEach((element) => {
 function changeVideo(videoID) {
   const iframe = document.getElementById("videoFrame");
   iframe.src = `https://www.youtube.com/embed/${videoID}`;
+}
+
+function loadLyrics(fileName) {
+  const lyricsContainer = document.getElementById("lyricsContainer");
+
+  fetch(fileName)
+    .then((response) => response.text())
+    .then((data) => {
+      lyricsContainer.textContent = data; // Display the lyrics in the container
+    })
+    .catch((error) => {
+      lyricsContainer.textContent = "Error loading lyrics. Please try again.";
+      console.error("Error fetching lyrics:", error);
+    });
+
+  // Highlight the active button
+  const buttons = document.querySelectorAll(".lyrics-buttons button");
+  buttons.forEach((button) => button.classList.remove("active"));
+  const activeButton = document.querySelector(
+    `button[onclick="loadLyrics('${fileName}')"]`
+  );
+  activeButton.classList.add("active");
 }
 
 document
