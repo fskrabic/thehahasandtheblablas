eba27e6ceb02f25eaa812581d5bb2e823f646c7c
F <filip.skrabic@protonmail.com>
2024-10-15 11:03:18 +0200

    or not

--- api/log-visit.js
+++ api/log-visit.js
@@ -10,13 +10,17 @@ const getIp = (req) => {
 };
 
 async function getGeolocation(ip) {
-  const url = `/api/proxy-ip-geo?ip=${ip}`;
+  const apiKey = process.env.IPGEOLOCATION_API_KEY;
+  const url = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}&ip=${ip}`;
+
   try {
     const response = await fetch(url);
     const data = await response.json();
+
     if (data && data.city && data.country_name) {
       return { city: data.city, country: data.country_name };
     }
+
     return null;
   } catch (error) {
     console.error("Error fetching geolocation:", error);
@@ -28,8 +32,11 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
       const ip = getIp(req);
+
       const location = await getGeolocation(ip);
+
       const { userAgent, referrer, pagePath } = req.body;
+
       const { error } = await supabase.from("page_visits").insert([
         {
           user_agent: userAgent,
@@ -39,9 +46,11 @@ export default async function handler(req, res) {
           country: location ? location.country : null,
         },
       ]);
+
       if (error) {
         throw error;
       }
+
       res.status(200).json({ message: "Visit logged successfully" });
     } catch (error) {
       console.error("Error logging visit:", error);
