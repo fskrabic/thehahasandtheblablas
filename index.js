12aed66e1b40af57c72bbba3590af168defe786d
filip <skrabicf@gmail.com>
2026-01-12 16:27:48 +0100

    ...

--- index.js
+++ index.js
@@ -202,6 +202,6 @@ function logVisitInfo() {
 //   window.style.visibility = "hidden";
 // }
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   new hahasApp();
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  new hahasApp();
+});
